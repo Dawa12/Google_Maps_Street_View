2c8fc21af00ff174363372f97da22f249e6ff1bb
Dawa Sherpa <dasherpa7@gmail.com>
2016-10-30 21:35:16 -0400

    can loop and now renders one map and street view

---
+++ models/favorites.js
@@ -0,0 +1,67 @@
+const { MongoClient } = require('mongodb');
+const { ObjectID } = require('mongodb');
+
+const dbConnection = 'mongodb://localhost:27017/maps';
+
+function getFavorite(req, res, next) {
+  MongoClient.connect(dbConnection, (err, db) => {
+    if (err) return next(err);
+
+    db.collection('favorites')
+    .find({})
+    .sort({ collectionName: 1})
+    .toArray((arrayError, data) => {
+      if (arrayError) return next(arrayError);
+// console.log('data is: ', data);
+      res.favorites = data;
+      db.close();
+      return next();
+    });
+    return false;
+  });
+  return false;
+}
+
+function saveFavorite(req, res, next) {
+  MongoClient.connect(dbConnection, (err, db) => {
+    if (err) return next(err);
+
+// console.log('showing req.body', req.body);
+
+    db.collection('favorites')
+      .insert(req.body.favorite, (insertErr, result) => {
+        if (insertErr) return next(insertErr);
+
+        res.saved = result;
+// console.log('inside saveFavorite function', result);
+        db.close();
+        return next();
+      });
+    return false;
+  });
+  return false;
+}
+
+
+function deleteFavorite(req, res, next) {
+  MongoClient.connect(dbConnection, (err, db) => {
+    if (err) return next(err);
+
+    db.collection('favorites')
+      .findAndRemove({ _id: ObjectID(req.params.id)}, (removeErr, doc) => {
+        if(removeErr) return next(removeErr);
+
+        res.removed = doc;
+        db.close();
+        return next();
+      });
+    return false;
+  })
+  return false;
+}
+
+module.exports = {
+  getFavorite,
+  saveFavorite,
+  // deleteFavorite,
+};
--- routes/map.js
+++ routes/map.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('index');
+  // console.log('index page router works');
+  res.render('index', {
+    lat: 40.7410605,
+    lng: -73.9896986,
+    favorites: res.favorites,
+  });
 });
 
 module.exports = router;
