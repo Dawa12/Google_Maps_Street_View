18da228d8d00f92fb8a8f0a331d892911bfa86cd
Dawa Sherpa <dasherpa7@gmail.com>
2016-11-01 09:37:08 -0400

    minor udpdate

--- routes/map.js
+++ routes/map.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
   // console.log('index page router works');
-  res.render('index2', {
+  res.render('index', {
     lat: 40.7410605,
     lng: -73.9896986,
     favorites: res.favorites,
